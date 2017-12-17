d5714bb50c1e78496cec6bf6d0922f3615e73fbe
SLMT <sam123456777@gmail.com>
2017-12-17 19:17:57 +0800

    [HW1] Finish problem 2

--- hw01/src/problem2.rs
+++ hw01/src/problem2.rs
@@ -4,8 +4,32 @@ pub type Matrix = Vec<Vec<f32>>;
 
 /// Computes the product of the inputs `mat1` and `mat2`.
 pub fn mat_mult(mat1: &Matrix, mat2: &Matrix) -> Matrix {
-    // TODO: Check the dimension, panic if they don't agree
+    // Check the dimension, panic if they don't agree
+    if mat1[0].len() != mat2.len() {
+        panic!("the dimensions do not agree.");
+    }
 
-    // TODO
-    unimplemented!();
+    let out_row = mat1.len();
+    let in_dim = mat1[0].len();
+    let out_col = mat2[0].len();
+
+    let mut result: Matrix = vec![];
+
+    for i in 0..out_row {
+        let mut row: Vec<f32> = vec![];
+
+        for j in 0..out_col {
+            let mut sum: f32 = 0.0;
+
+            for k in 0..in_dim {
+                sum += mat1[i][k] * mat2[k][j];
+            }
+
+            row.push(sum);
+        }
+
+        result.push(row);
+    }
+
+    result
 }
