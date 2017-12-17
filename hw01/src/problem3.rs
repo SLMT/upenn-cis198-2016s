6d993a0f4209158b3f0281258e192282b5e1aa40
SLMT <sam123456777@gmail.com>
2017-12-17 19:26:21 +0800

    [HW1] Finish problem 3

--- hw01/src/problem3.rs
+++ hw01/src/problem3.rs
@@ -2,6 +2,24 @@
 /// Find all prime numbers less than `n`.
 /// For example, `sieve(7)` should return `[2, 3, 5]`
 pub fn sieve(n: u32) -> Vec<u32> {
-    // TODO
-    unimplemented!();
+    let mut is_prime: Vec<bool> = vec![true; n as usize];
+    let mut result: Vec<u32> = vec![];
+
+    for num in 2..n {
+        if !is_prime[num as usize] {
+            continue;
+        }
+
+        result.push(num);
+
+        let mut mul: u32 = num;
+        let mut not_prime: u32 = mul * num;
+        while not_prime < n {
+            is_prime[not_prime as usize] = false;
+            mul += 1;
+            not_prime = mul * num;
+        }
+    }
+
+    result
 }
