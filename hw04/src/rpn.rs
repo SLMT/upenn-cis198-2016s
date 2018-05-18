089bf4d789a558c247a7580ad93b4f96015bb11c
SLMT <sam123456777@gmail.com>
2018-05-18 15:55:44 +0800

    [HW4] Finish hw4

---
+++ hw04/src/rpn.rs
@@ -0,0 +1,297 @@
+use std::result;
+use std::io;
+
+use rand;
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+/// An element of the stack. May be either integer or boolean.
+pub enum Elt {
+    Int(i32),
+    Bool(bool),
+}
+
+#[derive(Debug)]
+/// An RPN calculator error.
+pub enum Error {
+    /// Tried to pop from an empty stack.
+    Underflow,
+    /// Tried to operate on invalid types (e.g. 4 + true)
+    Type,
+    /// Unable to parse the input.
+    Syntax,
+    /// Some IO error occurred.
+    IO(io::Error),
+    /// The user quit the program (with `quit`).
+    Quit,
+}
+
+#[derive(Debug)]
+/// Types of RPN calculator operations.
+pub enum Op {
+    /// Adds two numbers: pop x, pop y, push x + y.
+    Add,
+    /// Checks equality of two values: pop x, pop y, push x == y.
+    Eq,
+    /// Negates a value: pop x, push ~x.
+    Neg,
+    /// Swaps two values: pop x, pop y, push x, push y.
+    Swap,
+    /// Computes a random number: pop x, push random number in [0, x).
+    Rand,
+    /// Quit the calculator.
+    Quit,
+}
+
+pub struct Stack {
+    elements: Vec<Elt>
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
+impl Stack {
+    /// Creates a new Stack
+    pub fn new() -> Stack {
+        Stack {
+            elements: Vec::new()
+        }
+    }
+
+    /// Pushes a value onto the stack.
+    pub fn push(&mut self, val: Elt) -> Result<()> {
+        self.elements.push(val);
+        Ok(())
+    }
+
+    /// Tries to pop a value off of the stack.
+    pub fn pop(&mut self) -> Result<Elt> {
+        match self.elements.pop() {
+            Some(e) => Ok(e),
+            None => Err(Error::Underflow)
+        }
+    }
+
+    /// Tries to evaluate an operator using values on the stack.
+    pub fn eval(&mut self, op: Op) -> Result<()> {
+        match op {
+            Op::Add => {
+                if let Elt::Int(x) = self.pop()? {
+                    if let Elt::Int(y) = self.pop()? {
+                        self.push(Elt::Int(x + y))
+                    } else {
+                        Err(Error::Type)
+                    }
+                } else {
+                    Err(Error::Type)
+                }
+            },
+            Op::Eq => {
+                match self.pop()? {
+                    Elt::Int(ix) => {
+                        match self.pop()? {
+                            Elt::Int(iy) => {
+                                self.push(Elt::Bool(ix == iy))
+                            },
+                            _ => {
+                                Err(Error::Type)
+                            }
+                        }
+                    },
+                    Elt::Bool(bx) => {
+                        match self.pop()? {
+                            Elt::Bool(by) => {
+                                self.push(Elt::Bool(bx == by))
+                            },
+                            _ => {
+                                Err(Error::Type)
+                            }
+                        }
+                    }
+                }
+            },
+            Op::Neg => {
+                match self.pop()? {
+                    Elt::Int(i) => {
+                        self.push(Elt::Int(-i))
+                    },
+                    Elt::Bool(b) => {
+                        self.push(Elt::Bool(!b))
+                    }
+                }
+            },
+            Op::Swap => {
+                let x = self.pop()?;
+                let y = self.pop()?;
+                self.push(x)?;
+                self.push(y)
+            },
+            Op::Rand => {
+                match self.pop()? {
+                    Elt::Int(x) => {
+                        let mut r = rand::random::<i32>();
+                        r = ((r % x) + x) % x; // handle negtive values
+                        self.push(Elt::Int(r))
+                    },
+                    _ => {
+                        Err(Error::Type)
+                    }
+                }
+            },
+            Op::Quit => {
+                Err(Error::Quit)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pop_empty1() {
+        let mut s = Stack::new();
+
+        let res = s.pop();
+        assert!(res.is_err());
+        if let Err(Error::Underflow) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_pop_empty2() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(0)).unwrap();
+
+        let res = s.pop();
+        assert!(res.is_ok());
+
+        let res = s.pop();
+        assert!(res.is_err());
+        if let Err(Error::Underflow) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_add1() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        s.push(Elt::Int(1)).unwrap();
+
+        assert!(s.eval(Op::Add).is_ok());
+        assert_eq!(s.pop().unwrap(), Elt::Int(2));
+    }
+
+    #[test]
+    fn test_eval_add2() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        s.push(Elt::Bool(false)).unwrap();
+
+        let res = s.eval(Op::Add);
+        assert!(res.is_err());
+        if let Err(Error::Type) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_add3() {
+        let mut s = Stack::new();
+        s.push(Elt::Bool(true)).unwrap();
+        s.push(Elt::Bool(false)).unwrap();
+
+        let res = s.eval(Op::Add);
+        assert!(res.is_err());
+        if let Err(Error::Type) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_eq1() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        s.push(Elt::Int(1)).unwrap();
+
+        assert!(s.eval(Op::Eq).is_ok());
+        assert_eq!(s.pop().unwrap(), Elt::Bool(true));
+    }
+
+    #[test]
+    fn test_eval_eq2() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        s.push(Elt::Bool(false)).unwrap();
+
+        let res = s.eval(Op::Eq);
+        assert!(res.is_err());
+        if let Err(Error::Type) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_neg1() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        assert!(s.eval(Op::Neg).is_ok());
+        assert_eq!(s.pop().unwrap(), Elt::Int(-1));
+    }
+
+    #[test]
+    fn test_eval_neg2() {
+        let mut s = Stack::new();
+        s.push(Elt::Bool(false)).unwrap();
+        assert!(s.eval(Op::Neg).is_ok());
+        assert_eq!(s.pop().unwrap(), Elt::Bool(true));
+    }
+
+    #[test]
+    fn test_eval_swap1() {
+        let mut s = Stack::new();
+        s.push(Elt::Int(1)).unwrap();
+        s.push(Elt::Bool(false)).unwrap();
+
+        assert!(s.eval(Op::Swap).is_ok());
+        assert_eq!(s.pop().unwrap(), Elt::Int(1));
+        assert_eq!(s.pop().unwrap(), Elt::Bool(false));
+
+        let res = s.pop();
+        assert!(res.is_err());
+        if let Err(Error::Underflow) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_swap2() {
+        let mut s = Stack::new();
+        s.push(Elt::Bool(false)).unwrap();
+
+        let res = s.eval(Op::Swap);
+        assert!(res.is_err());
+        if let Err(Error::Underflow) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_rand1() {
+        let mut s = Stack::new();
+        let i = 20;
+        s.push(Elt::Int(i)).unwrap();
+
+        assert!(s.eval(Op::Rand).is_ok());
+
+        let rand_val = s.pop().unwrap();
+        assert!(rand_val >= Elt::Int(0));
+        assert!(rand_val < Elt::Int(i));
+    }
+
+    #[test]
+    fn test_eval_rand2() {
+        let mut s = Stack::new();
+        s.push(Elt::Bool(false)).unwrap();
+
+        let res = s.eval(Op::Rand);
+        assert!(res.is_err());
+        if let Err(Error::Type) = res { } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_eval_quit() {
+        let mut s = Stack::new();
+
+        let res = s.eval(Op::Quit);
+        assert!(res.is_err());
+        if let Err(Error::Quit) = res { } else { assert!(false); }
+    }
+}
