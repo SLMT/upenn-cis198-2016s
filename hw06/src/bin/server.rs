40cf89530d240c0c5e94edc3209e0e733ecf377c
SLMT <sam123456777@gmail.com>
2018-06-06 22:19:32 +0800

    [HW6] Fix the server

--- hw06/src/bin/server.rs
+++ hw06/src/bin/server.rs
@@ -1,14 +1,15 @@
 extern crate hyper;
-extern crate rustc_serialize;
 extern crate bbs;
 
 use std::fs::{File, OpenOptions};
 use std::io::{self, Read, Write};
 use std::net::TcpStream;
+use std::net::SocketAddr;
 
-use hyper::server::{Request, Response, Server};
-use hyper::status::StatusCode;
-use rustc_serialize::json;
+use hyper::{Body, Method, Server, StatusCode, Request, Response};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
 use bbs::Message;
 use bbs::{SERVER_ADDR, BOT_ADDR, HTML_DATA, HTML_HEADER, HTML_FOOTER};
 
@@ -24,9 +25,11 @@ macro_rules! try_or_server_err {
     })
 }
 
-fn req_handler(mut req: Request, mut res: Response) {
-    match req.method {
-        hyper::Get => {
+fn req_handler(req: Request<Body>) -> Response<Body> {
+    let mut resp = Response::builder();
+
+    match *req.method() {
+        Method::GET => {
             // Read the files [HTML_HEADER, HTML_DATA, HTML_FOOTER] into buf.
             // If HTML_DATA doesn't exist, it should be skipped without failure.
             // Use `try_or_server_err!(expression, res)` instead of `try!(expression)` in
@@ -35,24 +38,24 @@ fn req_handler(mut req: Request, mut res: Response) {
             // TODO
 
             // And return buf as the response.
-            *res.status_mut() = StatusCode::Ok;
-            res.send(&buf.as_bytes()).unwrap();
+            resp.status(StatusCode::OK).body(Body::from("Hello")).expect("send response error")
         },
-        hyper::Post => {
+        Method::POST => {
             // Read the message out of the `req` into a buffer, handle it, and respond with Ok.
             // TODO
+            resp.status(StatusCode::NOT_FOUND).body(Body::from("No Post")).expect("send response error")
         },
-        _ => *res.status_mut() = StatusCode::ImATeapot,
+        _ => resp.status(StatusCode::IM_A_TEAPOT).body(Body::from("It's a teapot.")).expect("send response error"),
     }
 }
 
 fn main() {
     println!("Listening on {}.", SERVER_ADDR);
-    match Server::http(SERVER_ADDR) {
-        Ok(server) => match server.handle(req_handler) {
-            Ok(_) => (),
-            Err(e) => println!("{:?}", e),
-        },
-        Err(e) => println!("{:?}", e),
-    }
+    let address = SERVER_ADDR.parse().expect("parse error for server address");
+    let server = Server::bind(&address)
+        .serve(|| service_fn_ok(req_handler))
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    // Run this server
+    hyper::rt::run(server);
 }
--- hw06/src/lib.rs
+++ hw06/src/lib.rs
@@ -1,11 +1,12 @@
-extern crate rustc_serialize;
 extern crate hyper;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate futures;
 
-use std::io::Read;
-use rustc_serialize::json;
-use rustc_serialize::{Encodable, Encoder};
-
-use hyper::status::StatusCode;
+use hyper::{Body, StatusCode, Uri, Result};
+use hyper::rt::{Future, Stream};
+use hyper::client::{Client, HttpConnector};
 
 pub const SERVER_ADDR: &'static str = "127.0.0.1:1980";
 pub const BOT_ADDR: &'static str = "127.0.0.1:1981";
@@ -16,7 +17,7 @@ pub const HTML_HEADER: &'static str = "html/header.html";
 pub const HTML_FOOTER: &'static str = "html/footer.html";
 
 
-#[derive(Debug, RustcDecodable, RustcEncodable)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     pub user: String,
     pub text: String,
@@ -34,7 +35,7 @@ impl Message {
 pub struct UserClient {
     username: String,
     server_addr: String,
-    client: hyper::Client,
+    client: Client<HttpConnector, Body>,
 }
 
 impl UserClient {
@@ -48,10 +49,14 @@ impl UserClient {
 
     // TODO: Implement send_msg
 
-    pub fn get_content(&self) -> hyper::Result<(StatusCode, String)> {
-        let mut response = try!(self.client.get(&self.server_addr).send());
-        let mut buf = String::new();
-        response.read_to_string(&mut buf).unwrap();
-        Ok((response.status, buf))
+    pub fn get_content(&self) -> Result<(StatusCode, String)> {
+        let response = self.client.get(self.server_addr.parse::<Uri>().unwrap()).wait()?;
+        let status = response.status();
+        let chunks = response.into_body().collect().wait()?;
+        let string = chunks.iter().fold(String::new(), |mut buf, chunk| {
+            buf.push_str(std::str::from_utf8(chunk).unwrap());
+            buf
+        });
+        Ok((status, string))
     }
 }
